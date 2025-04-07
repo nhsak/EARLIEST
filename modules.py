958348cca8feca63577fc55b93abfb636b4ae79c
nhsak <mra1510@outlook.com>
2025-04-07 05:57:59 +0200

    Adjust modules to same parameters as TS handling

--- modules.py
+++ modules.py
@@ -1,25 +1,26 @@
+from torch import nn
 import torch
-import torch.nn as nn
 from torch.distributions import Bernoulli
 
 class BaselineNetwork(nn.Module):
     """
     A network which predicts the average reward observed
-    during a Markov decision-making process.
+    during a markov decision-making process.
     Weights are updated w.r.t. the mean squared error between
     its prediction and the observed reward.
     """
+
     def __init__(self, input_size, output_size):
         super(BaselineNetwork, self).__init__()
 
         # --- Mappings ---
         self.fc = nn.Linear(input_size, output_size)
 
+        
+
     def forward(self, x):
-        # Detach the input for the baseline network to avoid gradient propagation
         b = self.fc(x.detach())
         return b
-
 
 class Controller(nn.Module):
     """
@@ -32,23 +33,10 @@ class Controller(nn.Module):
         # --- Mappings ---
         self.fc = nn.Linear(ninp, nout)  # Optimized w.r.t. reward
 
-        torch.nn.init.normal_(self.fc.bias, mean=-10, std=1e-1)
-
-        
-
     def forward(self, x):
-        # Pass input through the controller network
-        
         probs = torch.sigmoid(self.fc(x))
-        print(self.fc.bias)
-        
-        # Exploration vs exploitation adjustment
-        probs = (1 - self._epsilon) * probs + self._epsilon * torch.FloatTensor([0.05]).to("cuda")  
-        
-        # Bernoulli distribution for the action
+        probs = (1-self._epsilon)*probs + self._epsilon*torch.FloatTensor([0.01])  # Explore/exploit
         m = Bernoulli(probs=probs)
-        action = m.sample()  # Sample an action
-        log_pi = m.log_prob(action)  # Compute log probability of sampled action
-        
-        # Return the action, log probability, and the negative log probability (for loss calculation)
+        action = m.sample() # sample an action
+        log_pi = m.log_prob(action) # compute log probability of sampled action
         return action.squeeze(0), log_pi.squeeze(0), -torch.log(probs).squeeze(0)
